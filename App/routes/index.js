d6676fa1263e2db262ff01423910841af1e34a84
minhhung3815 <77311655+minhhung3815@users.noreply.github.com>
2023-01-30 09:42:23 +0700

    Update index.js

--- App/routes/index.js
+++ App/routes/index.js
@@ -6,6 +6,9 @@ const user = require('./userRoute')
 
 route.use('/api/v1/user', user)
 route.use('/api/v1/accommodation', accommodation)
+route.use((req,res) => {
+return res.status(404).json({success:false, data: "Not found"});
+})
 
 
 module.exports = route
